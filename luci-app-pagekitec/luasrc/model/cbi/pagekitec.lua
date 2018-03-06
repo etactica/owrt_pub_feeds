c8b04381190f89802fcdab1c075be5c88f7a75cd
Karl Palsson <karlp@etactica.com>
2018-03-06 14:02:06 +0000

    pagekite: update to procd init scripts
    Add support for "static" the usecase of having your own private frontend
    running.  Reworded help and intro text obsoleted when moving from
    pagekite.py to pagekitec

--- luci-app-pagekitec/luasrc/model/cbi/pagekitec.lua
+++ luci-app-pagekitec/luasrc/model/cbi/pagekitec.lua
@@ -1,23 +1,10 @@
---[[
-LuCI model for pagekite configuration management
-Copyright OpenWrt.org, 2012
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-]]--
-
 m = Map("pagekitec", "PageKite",
     [[
-<p/>Note: you need a working PageKite account for this form to work. 
+<p/>Note: you need a working PageKite account, or at least, your own running front end for this form to work.
 Visit <a href="https://pagekite.net/home/">your account</a> to set up a name for your
 router and get a secret key for the connection.
 <p/><em>Note: this web configurator only supports
-some very very basic uses of pagekite.  For more complex uses, disable this
-page, and edit /etc/pagekite.d/pagekite.rc directly.</em>
+some very very basic uses of pagekite.</em>
 ]])
  
 s = m:section(TypedSection, "pagekitec", "PageKite")
@@ -26,6 +13,9 @@ s.anonymous = true
 p = s:option(Value, "kitename", "Kite Name")
 p = s:option(Value, "kitesecret", "Kite Secret")
 p.password = true
+p = s:option(Flag, "static", "Static Setup",
+	[[Static setup, disable FE failover and DDNS updates, set this if you are running your
+	own frontend without a pagekite.me account]])
 
 p = s:option(Flag, "simple_http", "Basic HTTP",
     [[Enable a tunnel to the local HTTP server (in most cases, this admin
