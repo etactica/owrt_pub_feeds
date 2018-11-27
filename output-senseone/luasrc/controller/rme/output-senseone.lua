3b69a04e7eb89f03f48073748ef94e9de98ac0e2
Karl Palsson <karlp@etactica.com>
2018-11-27 16:23:37 +0000

    output-senseone: include diags page support
    Senseone just needs a bridge to check, and the default bridge name
    (senseone) is sufficient.

--- output-senseone/luasrc/controller/rme/output-senseone.lua
+++ output-senseone/luasrc/controller/rme/output-senseone.lua
@@ -4,7 +4,17 @@
 module("luci.controller.rme.output-senseone", package.seeall)
 
 function index()
-    entry({"admin", "services", "output-senseone"}, cbi("rme/output-senseone"), "Output-SenseOne", 20)
+	entry({"admin", "services", "output-senseone"}, cbi("rme/output-senseone"), "Output-SenseOne", 20)
+	entry({"admin", "services", "output-senseone", "diag"}, call("action_diag"), nil)
+end
+
+function action_diag()
+	local rval = {
+		friendly_name = "SenseOne IoT Platform",
+		expect_bridge = true,
+		expect_process = false,
+	}
+	return luci.http.write_json(rval)
 end
 
 
