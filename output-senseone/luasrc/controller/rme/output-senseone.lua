545db48744b5b46dd9e46394de858a4fc39d2bb2
Karl Palsson <karlp@etactica.com>
2019-02-07 15:30:24 +0000

    output-senseone: handle custom bridge notification topics
    Diagnostic output was broken after the senseone bridge notifications
    were moved to a custom topic.
    
    Fixes GATE-1151

--- output-senseone/luasrc/controller/rme/output-senseone.lua
+++ output-senseone/luasrc/controller/rme/output-senseone.lua
@@ -2,6 +2,8 @@
 --Karl Palsson, <karlp@etactica.com> Sept 2018
 --]]
 module("luci.controller.rme.output-senseone", package.seeall)
+local ru = require("remake.utils")
+local uci = require("luci.model.uci")
 
 function index()
 	entry({"admin", "services", "output-senseone"}, cbi("rme/output-senseone"), "Output-SenseOne", 20)
@@ -9,10 +11,14 @@ function index()
 end
 
 function action_diag()
+	local gateid = ru.lookup_source_token()
+	local cursor = uci.cursor()
+	local username = cursor:get_first("output-senseone", "general", "username")
 	local rval = {
 		friendly_name = "SenseOne IoT Platform",
 		expect_bridge = true,
 		expect_process = false,
+		bridge_notification_topic = string.format("%s/bridge/%s/state", username, gateid),
 	}
 	return luci.http.write_json(rval)
 end
