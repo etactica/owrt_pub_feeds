5cb332d00deb64004d34f6106929c2c78da1c2b4
Karl Palsson <karlp@etactica.com>
2019-04-01 17:03:49 +0000

    output-statsd: drop irrelvant ntp actions

--- output-statsd/files/etc/hotplug.d/ntp/25-output-statsd-ntp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. /lib/functions/procd.sh
-
-[ "$ACTION" = stratum ] || exit 0
-
-/etc/init.d/output-statsd enabled && {
-	logger -t output-statsd "starting on NTP stratum event"
-	/etc/init.d/output-statsd start
-}
