0e8058721371db2b6f50b438cf2f60ff2305c1d8
Karl Palsson <karlp@etactica.com>
2020-02-21 17:36:18 +0000

    output-statsd: support at least basic default diags
    Fixes GATE-1244

--- output-statsd/luasrc/controller/rme/output-statsd.lua
+++ output-statsd/luasrc/controller/rme/output-statsd.lua
@@ -2,6 +2,7 @@
 --Karl Palsson, <karlp@etactica.com> Sept 2018
 --]]
 module("luci.controller.rme.output-statsd", package.seeall)
+local pl = require("pl.import_into")()
 
 function index()
     entry({"admin", "services", "output-statsd"}, cbi("rme/output-statsd"), "Output-StatsD", 20)
@@ -9,10 +10,12 @@ function index()
 end
 
 function action_diag()
+    local _, code = pl.utils.execute("pidof output-statsd.lua 2>&1 >/dev/null")
     local rval = {
         friendly_name = "StatsD live stream exporter",
         expect_bridge = false,
         expect_process = true,
+        process = code == 0,
     }
     return luci.http.write_json(rval)
 end
