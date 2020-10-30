3023752b20070201946ce8659d1e9add7d2d05ec
Karl Palsson <karlp@tweak.net.au>
2020-10-30 13:21:35 +0000

    klappir/senseone: less logging spam
    If the service isn't enabled, don't spam the log starting up and doing
    nothing.
    
    Fixes: GATE-1287

--- output-klappir/files/etc/hotplug.d/ntp/25-output-klappir-ntp.sh
+++ output-klappir/files/etc/hotplug.d/ntp/25-output-klappir-ntp.sh
@@ -3,7 +3,9 @@
 
 [ "$ACTION" = stratum ] || exit 0
 
-/etc/init.d/output-klappir enabled && {
-	logger -t output-klappir "starting on NTP stratum event"
-	/etc/init.d/output-klappir start
-}
+# Ignore if not enabled
+[ $(uci get output-klappir.@general[0].enabled) -eq 0 ] && exit 0
+/etc/init.d/output-klappir enabled || exit 0
+
+logger -t output-klappir "starting on NTP stratum event"
+/etc/init.d/output-klappir start "NTP Stratum event"
--- output-senseone/files/etc/hotplug.d/ntp/25-output-senseone-ntp.sh
+++ output-senseone/files/etc/hotplug.d/ntp/25-output-senseone-ntp.sh
@@ -3,6 +3,10 @@ APP=output-senseone
 
 case "$ACTION" in
 	stratum)
+		# Ignore if not enabled
+		[ $(uci get $APP.@general[0].enabled) -eq 0 ] && exit 0
+		/etc/init.d/$APP enabled || exit 0
+
 		logger -t $APP "Starting on NTP stratum event"
 		/etc/init.d/$APP start
 		;;
