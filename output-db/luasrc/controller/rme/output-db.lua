2beb656571fc28d4d94406fc6734ded1f29de1dc
Karl Palsson <karlp@etactica.com>
2021-10-06 17:21:13 +0000

    output-db: support diagnostics per instance
    Now that core diags supports multiple instances, we should really make
    sure that the backend knows how to provide multi instance diags as well.
    
    Fixes: GATE-1234

--- output-db/luasrc/controller/rme/output-db.lua
+++ output-db/luasrc/controller/rme/output-db.lua
@@ -2,17 +2,52 @@
 --Karl Palsson, <karlp@etactica.com> Nov 2019
 --]]
 module("luci.controller.rme.output-db", package.seeall)
+local pl = require("pl.import_into")()
 
 function index()
     entry({"admin", "services", "output-db"}, view("rme/output-db"), _("Output-Database"), 20)
-    entry({"admin", "services", "output-db", "diag"}, call("action_diag"), nil)
+    entry({"admin", "services", "output-db", "diag"}, call("action_diag"), nil).leaf = true
 end
 
 function action_diag()
+	local pathinfo = luci.http.getenv("PATH_INFO") -- from luci
+	local chunks = pl.stringx.split(luci.http.urldecode(pathinfo), "/")
+	--"/admin/services/output-db/diag/instancename"
+	if #chunks ~= 6 then
+		luci.http.status(400, "Illegal Argument")
+		return luci.http.write_json({
+			err = "Diags without an instance id are unsupported",
+			description = "You must provide an instance id as the last url fragment",
+		})
+	end
+	local instance = chunks[6]
+
+	-- Jump through some hoops to prevent executing user code, even though
+	-- to reach this endpoint, they must be logged in with root credentials.
+	-- The instance name itself is still user controlled, but it's checked
+	-- by luci itself elsewhere.
+	local cursor = uci.cursor()
+	local allowed_instances = {}
+	cursor:foreach("output-db", "instance", function(s)
+		table.insert(allowed_instances, s[".name"])
+	end)
+
+	local allowed = pl.tablex.find(allowed_instances, instance)
+	if not allowed then
+		luci.http.status(400, "Illegal Argument")
+		return luci.http.write_json({
+			err = "Diags requested for an unknown instanceid",
+			description = "You must provide a known and valid instance id as the last url fragment",
+		})
+	end
+
+	local _, code = pl.utils.execute(string.format("pgrep -f 'output-db.lua -i %s' >/dev/null 2>&1", instance))
+	local process = code == 0
     local rval = {
         friendly_name = "Basic database exporter",
         expect_bridge = false,
         expect_process = true,
+        process = process,
     }
     return luci.http.write_json(rval)
 end
