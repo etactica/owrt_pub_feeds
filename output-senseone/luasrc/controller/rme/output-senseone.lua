8c5ab9482d0d83bc61c71989e28ecdb89efff9a0
Karl Palsson <karlp@etactica.com>
2022-10-26 14:16:34 +0000

    output-senseone: properly support diags on new process
    This also unifies the /usr/bin names to be without the .lua suffix, as
    we have done elsewhere.

--- output-senseone/luasrc/controller/rme/output-senseone.lua
+++ output-senseone/luasrc/controller/rme/output-senseone.lua
@@ -2,6 +2,7 @@
 --Karl Palsson, <karlp@etactica.com> Sept 2018
 --]]
 module("luci.controller.rme.output-senseone", package.seeall)
+local pl = require("pl.import_into")()
 local ru = require("remake.utils")
 local uci = require("luci.model.uci")
 
@@ -11,13 +12,16 @@ function index()
 end
 
 function action_diag()
+	local _, code = pl.utils.execute("pidof output-senseone 2>&1 >/dev/null")
+	local process = code == 0
 	local gateid = ru.lookup_source_token()
 	local cursor = uci.cursor()
 	local username = cursor:get_first("output-senseone", "general", "username")
 	local rval = {
 		friendly_name = "SenseOne IoT Platform",
+		process = process,
 		expect_bridge = true,
-		expect_process = false,
+		expect_process = true,
 		bridge_notification_topic = string.format("%s/bridge/%s/state", username, gateid),
 	}
 	return luci.http.write_json(rval)
